d1caa2e269bffdf6bf81730fddc7fa8f4ddecda2
Jiashuai Shi <shijiashuai@gmail.com>
2016-09-11 14:23:27 +0800

    添加大学,地方卫视,粤语特征,地区0.650

--- tfidf.py
+++ tfidf.py
@@ -12,32 +12,80 @@ from utils import maps, preprocessor
 import numpy as np
 import json
 
+
+def predict_birthday(x_train, y_train, x_test):
+    vectorizer = CountVectorizer()
+    train_vector = vectorizer.fit_transform(x_train)
+    test_vector = vectorizer.transform(x_test)
+    tuned_params = [{'alpha': [0.001, 0.1, 1, 10]}]
+    clf = GridSearchCV(MultinomialNB(), tuned_params, cv=5)
+    clf.fit(train_vector, y_train)
+    print clf.best_params_
+    clf = clf.best_estimator_
+    print classification_report(y_train, clf.predict(train_vector))
+    return clf.predict(test_vector)
+
+
+def predict_gender(x_train, y_train, x_test):
+    vectorizer = CountVectorizer()
+    train_vector = vectorizer.fit_transform(x_train)
+    test_vector = vectorizer.transform(x_test)
+    tuned_params = [{'alpha': [0.001, 0.1, 1, 10]}]
+    clf = GridSearchCV(MultinomialNB(), tuned_params, cv=5)
+    clf.fit(train_vector, y_train)
+    print clf.best_params_
+    clf = clf.best_estimator_
+    print classification_report(y_train, clf.predict(train_vector))
+    return clf.predict(test_vector)
+
+
+def predict_location(x_train, y_train, x_test):
+    with open('nlp/dict_definited/locationFeatures.txt') as loc_features:
+        area_set = set()
+        for line in loc_features.readlines():
+            if not line.startswith('#'):
+                area_set.add(line.strip().decode('utf-8'))
+        area_dict = [i for i in area_set]
+    vectorizer = CountVectorizer(vocabulary=area_dict, analyzer='char', ngram_range=(1, 8))
+    train_vector = vectorizer.fit_transform(x_train)
+    test_vector = vectorizer.transform(x_test)
+    tuned_params = [{'alpha': [0.001, 0.1, 1, 5, 10]}]
+    clf = GridSearchCV(MultinomialNB(), tuned_params, cv=5)
+    clf.fit(train_vector, y_train)
+    print clf.best_params_
+    clf = clf.best_estimator_
+    print classification_report(y_train, clf.predict(train_vector))
+    # for vec in test_vector:
+    #     print json.dumps([vectorizer.get_feature_names()[j] for j in vec.indices],
+    #                      ensure_ascii=False)
+    return clf.predict(test_vector)
+
+
 if __name__ == '__main__':
     with open('data/train/train_info.txt') as info, open('data/train/train_labels.txt') as labels, \
             open('data/train/train_links.txt') as links, open('data/train/train_status.txt') as status, \
             open('data/test/test_info.txt') as test_info, open('data/test/test_status.txt') as test_status, \
-            open('data/test/test_nolabels.txt') as test_no_labels, open(
-            'nlp/dict_definited/areaDict_fullName2.json') as dict_d, open(
-            'nlp/dict_definited/locationFeatures.txt') as loc_features:
-        # open('data/other_links.txt') as other_links, open('data/unlabeled_statuses.txt') as ul_status, \
+            open('data/test/test_nolabels.txt') as test_no_labels:
         # 读入训练集并进行预处理
         users = {}
-        for line in info:
-            uid = line.split('||')[0]
-            users[uid] = {}
+        # for line in info:
+        #     uid = line.split('||')[0]
+        #     users[uid] = {}
         for line in labels:
             infos = line.strip().split('||')
             uid = infos[0]
-            users[uid] = {'content': ''}
-            users[uid]['gender'] = infos[1]
-            users[uid]['birthday'] = maps.age2seg(int(infos[2]))
-            users[uid]['location'] = maps.province2area(infos[3].split(' ')[0].decode('utf-8'))
-            # if users[uid]['location'] != u'境外':
-            #     users[uid]['location'] = u'境内'
+            if not maps.province2area(infos[3].split(' ')[0].decode('utf-8')) == u'境外':
+                users[uid] = {'content': ''}
+                users[uid]['gender'] = infos[1]
+                users[uid]['birthday'] = maps.age2seg(int(infos[2]))
+                users[uid]['location'] = maps.province2area(infos[3].split(' ')[0].decode('utf-8'))
+                # if users[uid]['location'] != u'境外':
+                #     users[uid]['location'] = u'境内'
         for line in status:
             weibo = [t for t in line.strip().split(',')]
             uid = weibo[0]
-            users[uid]['content'] += preprocessor.weibo_process(weibo[5])
+            if uid in users:
+                users[uid]['content'] += preprocessor.weibo_process(weibo[5])
         # 读入测试集
         test_users = {}
         test_uid = []
@@ -72,69 +120,9 @@ if __name__ == '__main__':
         for uid in test_uid:
             test_content.append(test_users[uid]['content'])
 
-        # birthday_features = [fe.strip().decode('utf-8') for fe in birthday_features.readlines()]
-        vectorizer = CountVectorizer()
-        count_vector_train = vectorizer.fit_transform(content)
-        count_vector_test = vectorizer.transform(test_content)
-        area_set = set()
-        for i in loc_features.readlines():
-            area_set.add(i.strip().decode('utf-8'))
-        area_dict = [i for i in area_set]
-        # area_dict = json.load(dict_d, 'utf-8')
-        location_vectorizer = CountVectorizer(vocabulary=area_dict, analyzer='char', ngram_range=(2, 4))
-        location_train = location_vectorizer.fit_transform(content)
-        location_test = location_vectorizer.transform(test_content)
-        # ch2_location = SelectKBest(f_classif, k=1200)
-        # ch2_location.fit(location_train, location)
-        # location_train = ch2_location.transform(location_train)
-        # location_test = ch2_location.transform(location_test)
-        # location_train = count_vector_train
-        # location_test = count_vector_test
-
-
-        # ch2_gender = SelectKBest(chi2, k=300)
-        # ch2_gender.fit(count_vector_train, gender)
-        # gender_train = ch2_gender.transform(count_vector_train)
-        # gender_test = ch2_gender.transform(count_vector_test)
-        gender_train = count_vector_train
-        gender_test = count_vector_test
-
-        # ch2_birthday = SelectKBest(f_classif, k=100)
-        # ch2_birthday.fit(count_vector_train, birthday)
-        # birthday_train = ch2_birthday.transform(count_vector_train)
-        # birthday_test = ch2_birthday.transform(count_vector_test)
-        birthday_train = count_vector_train
-        birthday_test = count_vector_test
-        # location_feature = []
-        # ch2_location_support = ch2_location.get_support(indices=True)
-        # feature_names = vectorizer.get_feature_names()
-        # for i in ch2_location_support:
-        #     print feature_names[i]
-        # location_feature.append(feature)
-
-        clf = MultinomialNB()
-        clf.fit(gender_train, gender)
-        print classification_report(gender, clf.predict(gender_train))
-        # 用cross-validation寻找最优参数
-        tuned_params = [{'kernel': ['rbf'], 'C': [1, 10, 100], 'gamma': [0.01, 0.001, 0.0001]}]
-        # tuned_params = [{'alpha': [0.05, 1, 0.5, 10]}]
-        # clf2 = GridSearchCV(SVR(), tuned_params, cv=5, n_jobs=-1)
-        clf2 = MultinomialNB()
-        clf2.fit(birthday_train, birthday)
-        # print clf2.best_params_
-        # clf2 = clf2.best_estimator_
-        print classification_report(birthday, clf2.predict(birthday_train))
-        # 用cross-validation寻找最优参数
-        tuned_params = [{'alpha': [0.1, 1, 5, 10]}]
-        # tuned_params = [{'kernel': ['rbf'], 'C': [1, 10], 'gamma': [0.1, 0.01]}]
-        clf3 = GridSearchCV(MultinomialNB(), tuned_params, cv=5, n_jobs=-1)
-        clf3.fit(location_train, location)
-        print clf3.best_params_
-        clf3 = clf3.best_estimator_
-        print classification_report(location, clf3.predict(location_train))
-        gender_predict = clf.predict(gender_test)
-        birthday_predict = clf2.predict(birthday_test)
-        location_predict = clf3.predict(location_test)
+        gender_predict = predict_gender(content, gender, test_content)
+        birthday_predict = predict_birthday(content, birthday, test_content)
+        location_predict = predict_location(content, location, test_content)
         # location_prob = clf3.predict_proba(location_test)
         # location_prob = np.max(location_prob, axis=1)
         for i, uid in enumerate(test_uid):
@@ -145,7 +133,7 @@ if __name__ == '__main__':
             print '%s,%s,%s,%s' % (
                 uid, birthday_predict[i], gender, location_predict[i])
             #     print location_prob[i]
-            # print json.dumps([vectorizer.get_feature_names()[j] for j in birthday_test[i].indices],
+            # print json.dumps([location.get_feature_names()[j] for j in birthday_test[i].indices],
             #                  ensure_ascii=False)
 
             # tuned_params = [{'alpha': [0.01, 0.03, 0.1, 0.3, 1.0], 'fit_prior': [True]},
